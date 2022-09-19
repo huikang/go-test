f42b87dc03e31005e9eea8cae670a197db3feddb
cskh <hui.kang@hashicorp.com>
2022-09-19 00:11:59 -0400

    add generic

---
+++ generics/generics_test.go
@@ -0,0 +1,42 @@
+package generics
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestNonGenerics(t *testing.T) {
+	// Initialize a map for the integer values
+	ints := map[string]int64{
+		"first":  34,
+		"second": 12,
+	}
+
+	// Initialize a map for the float values
+	floats := map[string]float64{
+		"first":  35.98,
+		"second": 26.99,
+	}
+
+	fmt.Printf("Non-Generic Sums: %v and %v\n",
+		SumInts(ints),
+		SumFloats(floats))
+}
+
+func TestGenerics(t *testing.T) {
+	// Initialize a map for the integer values
+	ints := map[string]int64{
+		"first":  34,
+		"second": 12,
+	}
+
+	// Initialize a map for the float values
+	floats := map[string]float64{
+		"first":  35.98,
+		"second": 26.99,
+	}
+
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
+}
