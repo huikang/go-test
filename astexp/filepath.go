5f010bce0d0d260498601ed47aa811ce88bea99b
cskh <hui.kang@hashicorp.com>
2023-04-04 16:05:36 -0400

    Add test in-memdb

--- astexp/filepath.go
+++ astexp/filepath.go
@@ -2,10 +2,13 @@ package astexp
 
 import (
 	"fmt"
+	"go/ast"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/hashicorp/go-memdb"
 )
 
 func walkDir(dir string) {
@@ -49,4 +52,78 @@ func listTestFiles(dir string) []string {
 
 	log.Println("total number of file", count)
 	return results
+}
+
+// UnitTestKind is the type of unit test; currently the supported
+// type is “TestServer”, “TestAgent”
+type UnitTestKind int
+
+const (
+	UnitTestKindTestServer = iota
+	UnitTestKindTestAgent
+)
+
+func (u UnitTestKind) String() string {
+	return [...]string{"TestServer", "TestAgent"}[u]
+}
+
+// UnitTestCase represents a Test case, e.g., Test_HealthAPI
+type UnitTestCase struct {
+	Name     string
+	FileName string
+	// NameFileName is unique
+	NameFileName string
+	Kind         UnitTestKind
+	RootAstNode  ast.Node
+	// CallExprs is the list of functions called by the test function
+	CallExprs []string
+}
+
+const unitTestTableName = "test_cases"
+
+func newTestCasesDB() (*memdb.MemDB, error) {
+	// Create an in-mem DB schema for all test functions
+	schema := &memdb.DBSchema{
+		Tables: map[string]*memdb.TableSchema{
+			unitTestTableName: &memdb.TableSchema{
+				Name: unitTestTableName,
+				Indexes: map[string]*memdb.IndexSchema{
+					// "name": {
+					// 	Unique:  false,
+					// 	Indexer: &memdb.StringFieldIndex{Field: "Name"},
+					// },
+					// "filename": {
+					// 	Unique: false,
+					// },
+					"id": {
+						Name:    "id",
+						Unique:  true,
+						Indexer: &memdb.StringFieldIndex{Field: "NameFileName"},
+					},
+					"test_kind": {
+						Name:    "test_kind",
+						Unique:  false,
+						Indexer: &memdb.StringFieldIndex{Field: "Kind"},
+					},
+					"call_exprs": {
+						Name:   "call_exprs",
+						Unique: false,
+						Indexer: &memdb.StringSliceFieldIndex{
+							Field: "CallExprs",
+						},
+					},
+				},
+			},
+		},
+	}
+	err := schema.Validate()
+	if err != nil {
+		return nil, fmt.Errorf("error validating schema: %v", err)
+	}
+
+	db, err := memdb.NewMemDB(schema)
+	if err != nil {
+		return nil, fmt.Errorf("error creating memdb: %v", err)
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -5,11 +5,16 @@ go 1.18
 require (
 	github.com/armon/go-metrics v0.3.11
 	github.com/hashicorp/consul v1.12.0
+	github.com/hashicorp/go-memdb v1.3.2
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/tools v0.7.0
 )
 
 require (
 	github.com/DataDog/datadog-go v3.2.0+incompatible // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
